e7d4343716c05b353bc3aa12834819a047a65ee2
BorysovS <s.borisovbar@gmail.com>
2022-12-05 22:50:17 +0200

    finish

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const makeMarkupList = ingredients => {
+  return ingredients.map(ingridient => {
+    const ingridientEl = document.createElement('li');
+    ingridientEl.textContent = `${ingridient}`;
+    ingridientEl.classList.add('item');
+    return ingridientEl;
+  });
+}; 
+
+const markupList = makeMarkupList(ingredients);
+list.append(...markupList)
